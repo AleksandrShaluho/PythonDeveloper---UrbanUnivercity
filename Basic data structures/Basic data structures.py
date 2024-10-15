08756d1d9e55dd9324e869716aa626c78c98e895
Aleksandr <nvrhddhrvn@yandex.ru>
2024-10-15 09:41:26 +0300

    Create Basic data structures.py
    Basic data structures exercises

---
+++ Basic data structures/Basic data structures.py
@@ -0,0 +1,19 @@
+# Basic data structures - all exercises in one project
+
+#Exercise 1 - Arithmetic
+print("1st program")
+print(9**0.5*5)
+
+#Exercise 2 - Logics
+print("2st program")
+print(9.99>9.98 and 1000!=1000.1)
+
+#Exercise 3 - School riddle
+print("3st program")
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+
+#Exercise 4 - The first after the dot
+print("4st program")
+print(int(float("123.456")*10)%10)
