6882a678ea7b23f5f50fb4f93c904cf772cdc6b8
Александр Шалухо <Aleksandr@Shaluho.ru>
2024-10-31 16:26:03 +0300

    Added module_3_2

---
+++ Namespace/python_3_2.py
@@ -0,0 +1,21 @@
+def SendEmail(message, recipient, *, sender="university.help@gmail.com"):
+    domains=['.ru','.com','.net']
+    if all(domain not in sender for domain in domains) or '@' not in sender:
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+        return
+    elif all(domain not in recipient for domain in domains) or '@' not in recipient:
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+        return
+    if sender == recipient:
+        print(f'Нельзя отправить письмо самому себе!')
+        return
+    if sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    elif sender != 'university.help@gmail.com':
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+
+
+SendEmail('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+SendEmail('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+SendEmail('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+SendEmail('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
