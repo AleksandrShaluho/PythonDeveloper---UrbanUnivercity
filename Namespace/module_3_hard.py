23d6fd73bf8630a1d6e2e1d737cefabbb244136a
Aleksandr <nvrhddhrvn@yandex.ru>
2024-11-11 01:47:00 +0300

    Module_3_1 added

--- Namespace/module_3_hard.py
+++ Namespace/module_3_hard.py
@@ -1,17 +1,25 @@
 # нужна рекурсивная функция с условиями определяющими заход во вложенные структуры
 def CalculateStructureSum(DataStructure):
-  for each in DataStructure:
-    SubList = list(each)
-    if isinstance(each,SubList):
-      break
+  SumOfElements = 0
+  for element in DataStructure:
+  #почему float - потому что мы не знаем, будут ли только целые числа или попадутся дробные.
+    if isinstance(element, float) or isinstance(element, int):
+      SumOfElements +=element
+    elif isinstance(element,str):
+      SumOfElements +=len(element)
+    #else:
+    #  CalculateStructureSum(element)
+  return SumOfElements
 
-DataStructure = [
-  [1, 2, 3],
-  {'a': 4, 'b': 5},
-  (6, {'cube': 7, 'drum': 8}),
-  "Hello",
-  ((), [{(2, 'Urban', ('Urban2', 35))}])
-]
+DataStructure = [1.2,2,3,'mommy',(1,2)]
+
+#DataStructure = [
+#  [1, 2, 3],
+#  {'a': 4, 'b': 5},
+#  (6, {'cube': 7, 'drum': 8}),
+#  "Hello",
+#  ((), [{(2, 'Urban', ('Urban2', 35))}])
+#]
 
 print(CalculateStructureSum(DataStructure))
 #print(list(DataStructure))
