868c6a41254b376c5e96d03592b6dbacd86c8d26
Aleksandr <nvrhddhrvn@yandex.ru>
2024-11-20 01:10:32 +0300

    Module_3_1 added

---
+++ Modules and packages/module_4_1.py
@@ -0,0 +1,8 @@
+from fake_math import Divide as FakeDiv
+from true_math import Divide as TrueDiv
+
+print(FakeDiv(69,3))
+print(FakeDiv(3,0))
+print(TrueDiv(49,7))
+print(TrueDiv(15,0))
+
---
+++ Modules and packages/true_math.py
@@ -0,0 +1,5 @@
+from math import inf
+
+def Divide(first, second):
+    if second == 0: return inf
+    return first/second
