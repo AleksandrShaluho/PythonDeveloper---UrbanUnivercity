06e0e5bc5dbf2dcabe8424e69c0e56f65c7cd860
Aleksandr <nvrhddhrvn@yandex.ru>
2024-11-24 01:36:44 +0300

    Module_3_1 added

---
+++ Classes and objects/module_5_3.py
@@ -0,0 +1,25 @@
+from module_5_Class_House import *
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2)
+
+h1 = h1 + 10
+print(h1)
+print(h1 == h2)
+
+h1 += 10
+print(h1)
+
+h2 = 10+ h2
+print(h2)
+
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 != h2)
--- Classes and objects/module_5_Class_House.py
+++ Classes and objects/module_5_Class_House.py
@@ -7,12 +7,88 @@ class House:
         return f'Название: {self.name}, количество этажей: {self.NumberOfFloors}'
 
     def __len__(self):
-       return self.NumberOfFloors
+        return self.NumberOfFloors
 
-    def GoTo(self,newFloor):
+    def __eq__(self, other):
+        if isinstance(other, House):
+            if self.NumberOfFloors == other.NumberOfFloors:
+                return True
+            else:
+                return False
+        else:
+            return "Ошибка в типе данных. Для сравнения должен передаваться объект класса House"
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            if self.NumberOfFloors < other.NumberOfFloors:
+                return True
+            else:
+                return False
+        else:
+            return "Ошибка в типе данных. Для сравнения должен передаваться объект класса House"
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            if self.NumberOfFloors <= other.NumberOfFloors:
+                return True
+            else:
+                return False
+        else:
+            return "Ошибка в типе данных. Для сравнения должен передаваться объект класса House"
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            if self.NumberOfFloors > other.NumberOfFloors:
+                return True
+            else:
+                return False
+        else:
+            return "Ошибка в типе данных. Для сравнения должен передаваться объект класса House"
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            if self.NumberOfFloors >= other.NumberOfFloors:
+                return True
+            else:
+                return False
+        else:
+            return "Ошибка в типе данных. Для сравнения должен передаваться объект класса House"
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            if self.NumberOfFloors != other.NumberOfFloors:
+                return True
+            else:
+                return False
+        else:
+            return "Ошибка в типе данных. Для сравнения должен передаваться объект класса House"
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.NumberOfFloors = self.NumberOfFloors + value
+            return self
+        else:
+            return "Ошибка в типе данных. Тип данных передаваемого параметра должен быть int"
+
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.NumberOfFloors += value
+            return self
+        else:
+            return "Ошибка в типе данных. Тип данных передаваемого параметра должен быть int"
+
+# на данном примере можно увидеть, что python не будет сам выполнять проверку типов, даже если указать тип данных для аргумента функции.
+# Python воспримет это как желательныйЮ но необязательный. Если в коде программы указать value = 10.5 например, то ошибок python не увидит.
+    def __radd__(self, value: int):
+        if isinstance(value, int):
+            self.NumberOfFloors += value
+            return self
+        else:
+            return "Ошибка в типе данных. Тип данных передаваемого параметра должен быть int"
+
+    def GoTo(self, newFloor):
         if newFloor < 1 or newFloor > self.NumberOfFloors:
             print('Такого этажа не существует')
         else:
-            for i in range(1,newFloor+1):
+            for i in range(1, newFloor + 1):
                 print(f'Этаж {i}')
-
