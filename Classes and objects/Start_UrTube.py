c6936cc9def80bda2ff16022a67a9e4527451724
Александр Шалухо <Aleksandr@Shaluho.ru>
2024-12-07 20:24:27 +0300

    Added module_3_2

---
+++ Classes and objects/Start_UrTube.py
@@ -0,0 +1,27 @@
+from UrTube import *
+from Video import *
+
+ur = UrTube()
+
+v1 = Video('Лучший язык программирования 2024 года', 200)
+v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+v3 = Video('Лучший язык программирования 2024 года', 200)
+
+ur.add(v1, v2, v3)
+
+print(ur.get_videos('лучший'))
+print(ur.get_videos('ПРОГ'))
+
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+ur.watch_video('Для чего девушкам парень программист?')
+ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+print(ur.current_user)
+ur.watch_video('Лучший язык программирования 2024 года!')
+ur.log_in('vasya_pupkin', 'lolkekchebure')
+ur.log_in('vasya_pupkin', 'lolkekcheburek')
+print(ur.current_user.password)
+ur.log_out()
+print(ur.current_user)
