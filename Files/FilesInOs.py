31df26ffa84209e3c41af2c235f0cfc1d5e38971
Александр Шалухо <Aleksandr@Shaluho.ru>
2024-12-25 19:45:53 +0300

    Finished module_7_5 exercise (Os module)

---
+++ Files/FilesInOs.py
@@ -0,0 +1,12 @@
+import os
+import time
+
+directory = r'C:\Users\Александр\PycharmProjects\PythonDeveloper---UrbanUnivercity'
+
+for root, dirs, files in os.walk(directory, topdown=True):
+    for file in files:
+        file_path = os.path.join(root,file)
+        file_size = os.path.getsize(file_path)
+        parent_dir = root
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(os.path.getmtime(file_path)))
+        print(f'Файл: {file:30} Путь: {file_path}, Размер: {file_size}, Изменен: {formatted_time}, Папка: {parent_dir}')
