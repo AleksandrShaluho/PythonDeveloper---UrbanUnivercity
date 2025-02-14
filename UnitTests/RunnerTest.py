849e900b9c4eca4ee938efe3a21a9d63d195a9d4
Александр Шалухо <Aleksandr@Shaluho.ru>
2025-02-15 00:38:06 +0300

    Finished 12_1 exercise (simple unittests)

---
+++ UnitTests/RunnerTest.py
@@ -0,0 +1,26 @@
+import unittest
+import runner
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        my_runner = runner.Runner('Bolt')
+        for _ in range(10):
+            my_runner.walk()
+        self.assertEqual(my_runner.distance, 50)
+
+    def test_run(self):
+        my_runner = runner.Runner('Bolt')
+        for _ in range(10):
+            my_runner.run()
+        self.assertEqual(my_runner.distance, 100)
+
+    def test_challenge(self):
+        my_runner = runner.Runner('Bolt')
+        alien_runner = runner.Runner('Brezhnev')
+        for _ in range(10):
+            my_runner.run()
+            alien_runner.walk()
+        self.assertNotEqual(my_runner.distance, alien_runner.distance)
+
+if __name__ == '__main__':
+    unittest.main()
